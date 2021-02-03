c5996f4c58d5a615641c13e960692a2bc9a82ffc
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-03 12:06:05 +0100

    dev: added bfs

--- src/app/bfs/bfs.component.ts
+++ src/app/bfs/bfs.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bfs',
   templateUrl: './bfs.component.html',
-  styleUrls: ['./bfs.component.scss']
+  styleUrls: ['./bfs.component.scss'],
 })
 export class BfsComponent implements OnInit {
+  cs143 =
+    'https://web.stanford.edu/class/archive/cs/cs143/cs143.1128/lectures/03/Slides03.pdf';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
