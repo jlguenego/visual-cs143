3ee04bfba52ae4edc8dd44ce8c20c09292a5eddb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-04 11:02:00 +0100

    dev: added animation on collapse component

--- src/app/widget/collapse/collapse.component.ts
+++ src/app/widget/collapse/collapse.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  AUTO_STYLE,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
@@ -5,6 +12,21 @@ import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
   selector: 'app-collapse',
   templateUrl: './collapse.component.html',
   styleUrls: ['./collapse.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({ height: '0', visibility: 'hidden' }),
+        animate(
+          '0.5s ease-out',
+          style({ height: AUTO_STYLE, visibility: AUTO_STYLE })
+        ),
+      ]),
+      transition(':leave', [
+        style({ height: AUTO_STYLE, visibility: AUTO_STYLE }),
+        animate('0.5s ease-in', style({ height: '0', visibility: 'hidden' })),
+      ]),
+    ]),
+  ],
 })
 export class CollapseComponent implements OnInit {
   faChevronDown = faChevronDown;
