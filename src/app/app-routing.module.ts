2f62562f67e4f42565b5dba63a44c2d904bac762
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-05 15:07:45 +0100

    fix: preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,28 @@ import { MenuComponent } from './routes/menu/menu.component';
 import { LegalComponent } from './routes/legal/legal.component';
 import { HomeComponent } from './routes/home/home.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'legal', component: LegalComponent },
   { path: 'menu', component: MenuComponent },
-  { path: 'bfs', loadChildren: () => import('./bfs/bfs.module').then(m => m.BfsModule) },
-  { path: 'bfs1', loadChildren: () => import('./bfs1/bfs1.module').then(m => m.Bfs1Module) },
+  {
+    path: 'bfs',
+    loadChildren: () => import('./bfs/bfs.module').then((m) => m.BfsModule),
+  },
+  {
+    path: 'bfs1',
+    loadChildren: () => import('./bfs1/bfs1.module').then((m) => m.Bfs1Module),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
