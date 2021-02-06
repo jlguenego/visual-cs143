428c84db2cf5b68b2f069d46ddef2470439891cf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-06 15:38:59 +0100

    dev: removed bfs1-anim

--- src/app/bfs1/bfs1.module.ts
+++ src/app/bfs1/bfs1.module.ts
@@ -5,10 +5,9 @@ import { CommonModule } from '@angular/common';
 import { Bfs1RoutingModule } from './bfs1-routing.module';
 import { Bfs1Component } from './bfs1.component';
 import { WidgetModule } from '../widget/widget.module';
-import { Bfs1AnimComponent } from './bfs1-anim/bfs1-anim.component';
 
 @NgModule({
-  declarations: [Bfs1Component, Bfs1AnimComponent],
+  declarations: [Bfs1Component],
   imports: [CommonModule, Bfs1RoutingModule, WidgetModule, ReactiveFormsModule],
 })
 export class Bfs1Module {}
