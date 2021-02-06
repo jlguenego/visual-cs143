239bcb62d522113c92594340b4d133d8bbdb42ef
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-06 12:25:11 +0100

    dev: added showPrevNext boolean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,14 @@ export class AppComponent {
   faBars = faBars;
   faGithub = faGithub;
   showPrevNext = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (!(event instanceof NavigationEnd)) {
+        return;
+      }
+      this.showPrevNext = event.url !== '/';
+      console.log(event);
+    });
+  }
 }
