d2722c3c0c11567017d2f4ad5e9cdf344d7bf998
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-08 09:37:42 +0100

    fix: added scroll control to yeah component

--- src/app/widget/yeah/yeah.component.ts
+++ src/app/widget/yeah/yeah.component.ts
@@ -1,18 +1,41 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock';
 
 @Component({
   selector: 'app-yeah',
   templateUrl: './yeah.component.html',
   styleUrls: ['./yeah.component.scss'],
 })
-export class YeahComponent implements OnInit {
-  @Input() visible = false;
+export class YeahComponent implements OnInit, OnDestroy {
+  private pVisible = false;
 
-  constructor() {}
+  @Input()
+  get visible(): boolean {
+    return this.pVisible;
+  }
+  set visible(value: boolean) {
+    this.pVisible = value;
+    // scroll lock
+    if (this.pVisible) {
+      disableBodyScroll(this.elt.nativeElement);
+    } else {
+      enableBodyScroll(this.elt.nativeElement);
+    }
+  }
+
+  constructor(private elt: ElementRef<HTMLElement>) {}
 
   ngOnInit(): void {}
 
   clear(): void {
     this.visible = false;
+  }
+
+  ngOnDestroy(): void {
+    clearAllBodyScrollLocks();
   }
 }
