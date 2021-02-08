5418e31da637ab432cc77894c62c85cd619e7122
Jean-Louis GUENEGO <jlguenego@gmail.com>
2021-02-08 17:14:27 +0100

    dev: added example

--- src/app/dfs-better-than-bfs/dfs-better-than-bfs.component.ts
+++ src/app/dfs-better-than-bfs/dfs-better-than-bfs.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import {
+  defineTerminalAlphabet,
+  defineNonTerminalAlphabet,
+  CFGSpecifications,
+  ContextFreeGrammar,
+  CFGSpec,
+  Sentence,
+  getBFS1TreeAsync,
+  PartialParseTree,
+  dfs1GetChildren,
+  ParseSymbol,
+  testFnAsync,
+} from '@jlguenego/syntax-analysis';
+import { BFSTreeAsync, DFSTreeAsync, Tree } from '@jlguenego/tree';
+import { interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-dfs-better-than-bfs',
   templateUrl: './dfs-better-than-bfs.component.html',
-  styleUrls: ['./dfs-better-than-bfs.component.scss']
+  styleUrls: ['./dfs-better-than-bfs.component.scss'],
 })
 export class DfsBetterThanBfsComponent implements OnInit {
+  f = new FormGroup({
+    delay: new FormControl(50),
+  });
 
-  constructor() { }
+  cs143 =
+    'https://web.stanford.edu/class/archive/cs/cs143/cs143.1128/lectures/03/Slides03.pdf';
 
-  ngOnInit(): void {
+  dfsTree!: DFSTreeAsync<unknown>;
+
+  constructor() {
+    const t = defineTerminalAlphabet(['a', 'b', 'c'] as const);
+    const nt = defineNonTerminalAlphabet(['A'] as const);
+
+    const spec: CFGSpecifications<typeof t, typeof nt> = {
+      startSymbol: 'A',
+      productions: [
+        { LHS: 'A', RHS: ['A', 'a'] },
+        { LHS: 'A', RHS: ['A', 'b'] },
+        { LHS: 'A', RHS: ['c'] },
+      ],
+    };
+    const cfg1 = new ContextFreeGrammar(spec as CFGSpec, t, nt);
+    const sentence: Sentence = ['c', ...new Array(8).fill('a')].map((str) => ({
+      name: str,
+    }));
+    this.dfsTree = new DFSTreeAsync<PartialParseTree>(
+      new PartialParseTree(new Tree<ParseSymbol>(cfg1.startSymbol)),
+      testFnAsync(sentence),
+      async (ppt: PartialParseTree) => {
+        await timer(+this.f.value.delay).toPromise();
+        return dfs1GetChildren(sentence, cfg1)(ppt);
+      }
+    ) as DFSTreeAsync<unknown>;
   }
 
+  ngOnInit(): void {}
+
+  itemToString(item: unknown | null): string {
+    if (!item) {
+      return '';
+    }
+    const it = item as Tree<PartialParseTree>;
+    return it.node.sententialForm.toString();
+  }
 }
--- src/app/dfs-better-than-bfs/dfs-better-than-bfs.module.ts
+++ src/app/dfs-better-than-bfs/dfs-better-than-bfs.module.ts
@@ -1,15 +1,18 @@
+import { WidgetModule } from './../widget/widget.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DfsBetterThanBfsRoutingModule } from './dfs-better-than-bfs-routing.module';
 import { DfsBetterThanBfsComponent } from './dfs-better-than-bfs.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DfsBetterThanBfsComponent],
   imports: [
     CommonModule,
-    DfsBetterThanBfsRoutingModule
-  ]
+    DfsBetterThanBfsRoutingModule,
+    ReactiveFormsModule,
+    WidgetModule,
+  ],
 })
-export class DfsBetterThanBfsModule { }
+export class DfsBetterThanBfsModule {}
